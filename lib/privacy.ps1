1203e8d1c333be0ba0bf56288c7eec7e338d646c
da-moon <damoon.azarpazhooh@ryerson.ca>
2020-04-23 18:44:05 +0000

    lib/privacy.ps1 Safe-Set-ItemProperty

--- lib/privacy.ps1
+++ lib/privacy.ps1
@@ -25,13 +25,9 @@ Function DisableWiFiSense {
 Function DisableSmartScreen {
     info "Disabling SmartScreen Filter..."
     $path="HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer"
-    If (Test-Path "$path") {
-        Set-ItemProperty -Path "$path" -Name "SmartScreenEnabled" -Type String -Value "Off"
-    }
+    Safe-Set-ItemProperty "$path"  "SmartScreenEnabled"  String  "Off"
     $path="HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppHost"
-    If (Test-Path "$path") {
-        Set-ItemProperty -Path "$path" -Name "EnableWebContentEvaluation" -Type DWord -Value 0
-    }
+    Safe-Set-ItemProperty "$path"  "EnableWebContentEvaluation"  DWord  0
 	$edge = (Get-AppxPackage -AllUsers "Microsoft.MicrosoftEdge").PackageFamilyName
     $path="HKCU:\SOFTWARE\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\$edge\MicrosoftEdge\PhishingFilter"
     Create-Path-If-Not-Exists "$path"
@@ -40,38 +36,37 @@ Function DisableSmartScreen {
         "PreventOverride"
     )
     foreach($name in $names) {
-        Set-ItemProperty -Path "$path" -Name "$name" -Type DWord -Value 0
+        Safe-Set-ItemProperty "$path"  "$name"  DWord  0
     }
     success "Disabling SmartScreen Filter..."
 }
 Function DisableWebSearch {
     info "Disabling Bing Search in Start Menu..."
     $path="HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search"
-    If (Test-Path "$path") {
-        Set-ItemProperty -Path "$path" -Name "BingSearchEnabled" -Type DWord -Value 0
-    }
+    Safe-Set-ItemProperty "$path"  "BingSearchEnabled"  DWord  0
     Create-Path-If-Not-Exists "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search"
-	Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Name "DisableWebSearch" -Type DWord -Value 1
+    Safe-Set-ItemProperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search"  "DisableWebSearch"  DWord  1
     success "Disabling Bing Search in Start Menu..."
 }
 Function DisableBackgroundApps {
     info "Disabling Background application access..."
     $path="HKCU:\Software\Microsoft\Windows\CurrentVersion\BackgroundAccessApplications"
-    If (Test-Path "$path") {
-        Get-ChildItem "$path" | ForEach-Object {
-            Set-ItemProperty -Path $_.PsPath -Name "Disabled" -Type DWord -Value 1
-            Set-ItemProperty -Path $_.PsPath -Name "DisabledByUser" -Type DWord -Value 1
-        }
+    Get-ChildItem "$path" | ForEach-Object {
+        Safe-Set-ItemProperty $_.PsPath  "Disabled"  DWord  1
+        Safe-Set-ItemProperty $_.PsPath  "DisabledByUser"  DWord  1
     }
     success "Disabling Background application access..."
 }
 Function DisableLockScreenSpotlight {
     info "Disabling Lock screen spotlight..."
     $path="HKCU:\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager"
-    If (Test-Path "$path") {
-        Set-ItemProperty -Path "$path" -Name 'RotatingLockScreenEnabled' -Type DWord -Value 0
-        Set-ItemProperty -Path "$path" -Name 'RotatingLockScreenOverlayEnabled' -Type DWord -Value 0
-        Set-ItemProperty -Path "$path" -Name 'SubscribedContent-338387Enabled' -Type DWord -Value 0
+    $names=@(
+        "RotatingLockScreenEnabled" , 
+        "RotatingLockScreenOverlayEnabled",
+        "SubscribedContent-338387Enabled"
+    )
+    foreach($name in $names) {
+        Safe-Set-ItemProperty "$path"  "$name"  DWord  0
     }
     success "Disabling Lock screen spotlight..."
 }
@@ -83,24 +78,20 @@ Function DisableLocationTracking {
     }
     $paths.GetEnumerator() | ForEach-Object {
         $path=$_.Key
-        If (Test-Path "$path") {
-            Set-ItemProperty -Path "$path" -Name $_.Value -Type DWord -Value 0
-        }
+        Safe-Set-ItemProperty "$path"  $_.Value  DWord  0
     }
     success "Disabling Location Tracking..."
 }
 Function DisableMapUpdates {
     info "Disabling automatic Maps updates..."
     $path="HKLM:\SYSTEM\Maps"
-    If (Test-Path "$path") {
-        Set-ItemProperty -Path "$path" -Name "AutoUpdateEnabled" -Type DWord -Value 0
-    }
+    Safe-Set-ItemProperty "$path"  "AutoUpdateEnabled"  DWord  0
     success "Disabling automatic Maps updates..."
 }
 Function DisableFeedback {
 	info "Disabling Feedback..."
     Create-Path-If-Not-Exists "HKCU:\SOFTWARE\Microsoft\Siuf\Rules"
-	Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Siuf\Rules" -Name "NumberOfSIUFInPeriod" -Type DWord -Value 0
+    Safe-Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Siuf\Rules"  "NumberOfSIUFInPeriod"  DWord  0
 	success "Disabling Feedback..."
 }
 Function DisableAdvertisingID {
@@ -112,38 +103,34 @@ Function DisableAdvertisingID {
     $paths.GetEnumerator() | ForEach-Object {
         $path=$_.Key
         Create-Path-If-Not-Exists "$path"
-        If (Test-Path "$path") {
-            Set-ItemProperty -Path "$path" -Name $_.Value -Type DWord -Value 0
-        }
+        Safe-Set-ItemProperty "$path"  $_.Value  DWord  0
     }
 	success "Disabling Advertising ID..."
 }
 Function DisableCortana {
     info "Disabling Cortana..."
     Create-Path-If-Not-Exists "HKCU:\SOFTWARE\Microsoft\Personalization\Settings"
-	Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Personalization\Settings" -Name "AcceptedPrivacyPolicy" -Type DWord -Value 0
+    Safe-Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Personalization\Settings"  "AcceptedPrivacyPolicy"  DWord  0
     Create-Path-If-Not-Exists "HKCU:\SOFTWARE\Microsoft\InputPersonalization"
-	Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization" -Name "RestrictImplicitTextCollection" -Type DWord -Value 1
-	Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization" -Name "RestrictImplicitInkCollection" -Type DWord -Value 1
+    Safe-Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\InputPersonalization"  "RestrictImplicitTextCollection"  DWord  1
+    Safe-Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\InputPersonalization"  "RestrictImplicitInkCollection"  DWord  1
     Create-Path-If-Not-Exists "HKCU:\SOFTWARE\Microsoft\InputPersonalization\TrainedDataStore"	
-	Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization\TrainedDataStore" -Name "HarvestContacts" -Type DWord -Value 0
+    Safe-Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\InputPersonalization\TrainedDataStore"  "HarvestContacts"  DWord  0
     Create-Path-If-Not-Exists "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search"
-	Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Name "AllowCortana" -Type DWord -Value 0
+    Safe-Set-ItemProperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search"  "AllowCortana"  DWord  0
     success "Disabling Cortana..."
 }
 Function DisableErrorReporting {
     info "Disabling Error reporting..."
     $path="HKLM:\SOFTWARE\Microsoft\Windows\Windows Error Reporting"
-    If (Test-Path "$path") {
-        Set-ItemProperty -Path "$path" -Name "Disabled" -Type DWord -Value 1
-    }
+    Safe-Set-ItemProperty "$path"  "Disabled"  DWord  1
     success "Disabling Error reporting..."
 }
 Function DisableAutoLogger {
 	info "Removing AutoLogger file and restricting directory..."
 	$autoLoggerDir = "$env:PROGRAMDATA\Microsoft\Diagnosis\ETLLogs\AutoLogger"
 	If (Test-Path "$autoLoggerDir\AutoLogger-Diagtrack-Listener.etl") {
-		Remove-Item "$autoLoggerDir\AutoLogger-Diagtrack-Listener.etl"
+        Remove-Item  -Path "$autoLoggerDir\AutoLogger-Diagtrack-Listener.etl" -ErrorAction SilentlyContinue
 	}
 	icacls $autoLoggerDir /deny SYSTEM:`(OI`)`(CI`)F | Out-Null
 	success "Removing AutoLogger file and restricting directory..."
@@ -164,21 +151,19 @@ Function DisableWAPPush {
 Function DisableAppSuggestions {
     info "Disabling Application suggestions..."
     $path="HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager"
-    If (Test-Path "$path") {
-        $names=@(
-            "ContentDeliveryAllowed",
-            "OemPreInstalledAppsEnabled" ,
-            "PreInstalledAppsEnabled",
-            "PreInstalledAppsEverEnabled" ,
-            "SilentInstalledAppsEnabled" ,
-            "SubscribedContent-338389Enabled" ,
-            "SystemPaneSuggestionsEnabled" ,
-            "SubscribedContent-338388Enabled" 
-        )
-        foreach($name in $names) {
-            Set-ItemProperty -Path "$path" -Name "$name" -Type DWord -Value 0
-        }
+    $names=@(
+        "ContentDeliveryAllowed",
+        "OemPreInstalledAppsEnabled" ,
+        "PreInstalledAppsEnabled",
+        "PreInstalledAppsEverEnabled" ,
+        "SilentInstalledAppsEnabled" ,
+        "SubscribedContent-338389Enabled" ,
+        "SystemPaneSuggestionsEnabled" ,
+        "SubscribedContent-338388Enabled" 
+    )
+    foreach($name in $names) {
+        Safe-Set-ItemProperty "$path"  "$name"  DWord  0
     }
     Create-Path-If-Not-Exists "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent"
-	Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent" -Name "DisableWindowsConsumerFeatures" -Type DWord -Value 1
+    Safe-Set-ItemProperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent"  "DisableWindowsConsumerFeatures"  DWord  1
 }
